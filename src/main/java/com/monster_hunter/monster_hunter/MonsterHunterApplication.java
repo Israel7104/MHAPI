a4175516c3e3a6f611afc9c6c67c74b3f3ead12f
israel7104 <israel7104@gmail.com>
2026-01-31 04:47:31 +0100

    Inicio proyecto

---
+++ src/main/java/com/monster_hunter/monster_hunter/MonsterHunterApplication.java
@@ -0,0 +1,13 @@
+package com.monster_hunter.monster_hunter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonsterHunterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonsterHunterApplication.class, args);
+	}
+
+}
